c25e4376b64ce40ec05d98624d492a1b22c5fecf
SunnySt4r <door011@mail.ru>
2023-10-20 16:28:31 +0300

    feat: try to fit model

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,20 @@
 package org.example;
 
+import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.ru.RussianAnalyzer;
 import smile.classification.KNN;
 import smile.classification.SVM;
 import smile.data.DataFrame;
 import smile.data.SparseDataset;
+import smile.data.Tuple;
+import smile.data.type.*;
 import smile.data.vector.BaseVector;
 import smile.data.vector.IntVector;
 import smile.feature.extraction.BagOfWords;
 import smile.io.Read;
+import smile.io.Write;
+import smile.math.Function;
+import smile.math.kernel.GaussianKernel;
 import smile.math.matrix.Matrix;
 import smile.math.matrix.SparseMatrix;
 import smile.plot.swing.Histogram;
@@ -17,69 +23,99 @@ import smile.plot.swing.Plot;
 import smile.plot.swing.PlotGrid;
 import smile.stat.distribution.BernoulliDistribution;
 import smile.validation.metric.AUC;
+import weka.classifiers.Classifier;
 import weka.classifiers.bayes.NaiveBayesMultinomialText;
 import weka.classifiers.meta.Bagging;
+import weka.classifiers.pmml.consumer.SupportVectorMachineModel;
 import weka.core.Instances;
 import weka.core.Stopwords;
 import weka.core.pmml.jaxbbindings.ROC;
+import weka.core.pmml.jaxbbindings.SupportVectorMachine;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.*;
 
 
 public class App {
 
-    static Map<String, Integer> dictionary = new HashMap<>();
-    public static Set<String> positive = new HashSet<>();
-    public static Set<String> negative = new HashSet<>();
-    static Set<String> del = new HashSet<>();
-    static Scanner scanner = new Scanner(System.in);
+    static Map<String, String> dictionary = new HashMap<>();
 
-    public static void main( String[] args ) throws IOException, URISyntaxException, InterruptedException, InvocationTargetException {
-        DataFrame df = Read.csv("src/main/resources/train.csv");
-        DataFrame test = Read.csv("src/main/resources/test.csv");
-        System.out.println(df.size());
+    public static void main( String[] args ) throws IOException, URISyntaxException{
+        DataFrame train = Read.csv("src/main/resources/changed/train.csv");
+        DataFrame test = Read.csv("src/main/resources/changed/test.csv");
+        BagOfWords bagOfWords = BagOfWords.fit(train, s -> s.split(" "), 10, "V2");
 
-        RussianAnalyzer russianAnalyzer = new RussianAnalyzer();
-        System.out.println(russianAnalyzer.getStopwordSet());
+        DataFrame word2vec = train.stream().skip(2)
+                .map(tuple -> {
+                    String s = tuple.getString("V2");
+                    Tuple res = Tuple.of(new Object[]{tuple.get("V1") , bagOfWords.apply((s != null)? s : ""), tuple.get("V3")},
+                            new StructType(
+                                    new StructField("id", DataTypes.IntegerObjectType),
+                                    new StructField("text_vec", DataTypes.IntegerArrayType),
+                                    new StructField("sentiment", DataTypes.IntegerObjectType)));
 
-        for (String s : df.column("V2").toStringArray()) {
-            s = s.replaceAll("[^\u0400-\u04FF]", " ").replaceAll("[ \\t]{2,}", " ").toLowerCase();
-            boolean prevIsNot = false;
-            for (String w : s.split(" ")) {
+                    return res;
+                })
+                .collect(DataFrame.Collectors.collect());
 
-                if(w.equals("не")){
-                    prevIsNot = true;
-                    continue;
-                }
-                if(prevIsNot && !dictionary.containsKey("не " + w)){
-                    prevIsNot = false;
-//                    w = "не " + w;
-//                    System.out.println("не " + w);
-//                    int i = scanner.nextInt();
-//                    if(i==1){
-//                        positive.add("не " + w);
-//                        negative.add(w);
-//                    }else if(i==2){
-//                        negative.add("не " + w);
-//                        positive.add(w);
-//                    }
-                    dictionary.put("не " + w, 1);
-                }
-            }
+        DataFrame test2vec = test.stream().skip(2)
+                .map(tuple -> {
+                    String s = tuple.getString("V3");
+                    Tuple res = Tuple.of(new Object[]{tuple.get("V1") , bagOfWords.apply((s != null)? s : " ")},
+                            new StructType(
+                                    new StructField("id", DataTypes.IntegerObjectType),
+                                    new StructField("text_vec", DataTypes.IntegerArrayType)));
+
+                    return res;
+                })
+                .collect(DataFrame.Collectors.collect());
+
+
+        System.out.println(word2vec.toString());
+        System.out.println(word2vec.size());
+        System.out.println(train.size());
+
+
+//        Map<String, Integer> map = new HashMap<>();
+//        for(int i=0; i<word2vec.size(); i++){
+//            map.put(word2vec.get(i).get("sentiment").toString(), 1);
+//        }
+//        System.out.println(map);System.out.println(Arrays.toString(x[0]));
+        int[][] x = new int[word2vec.size()][bagOfWords.features().length];
+        int[] y = new int[word2vec.size()];
+
+        for(int i=0; i<word2vec.size(); i++){
+            x[i] = (int[]) word2vec.get(i).get("text_vec");
+            y[i] = Integer.parseInt(word2vec.get(i).get("sentiment").toString()) * 2 - 1;
         }
-        System.out.println(positive);
-        System.out.println("-------------");
-        System.out.println(negative);
+
+//        Classifier<int[]> svm = SVM.fit(x, y, 1, 0.1, 0.01);
+
+//        DataFrame result = test2vec.stream().skip(2)
+//                .map(tuple -> {
+//                    Tuple res = Tuple.of(new Object[]{tuple.get("id") , svm.predict((int[]) tuple.get("text_vec"))},
+//                            new StructType(
+//                                    new StructField("id", DataTypes.IntegerObjectType),
+//                                    new StructField("sentiment", DataTypes.IntegerObjectType)));
+//
+//                    return res;
+//                })
+//                .collect(DataFrame.Collectors.collect());
+//
+//        Write.csv(result, Paths.get("src/main/resources/changed/result.csv"));
 
 
-//        BagOfWords bagOfWords = BagOfWords.fit(df, s -> s
-//                .replaceAll("[^\u0400-\u04FF]", " ")
-//                .replaceAll("[ \\t]{2,}", " ")
-//                .toLowerCase().split(" "), 1000, "V2");
+//        word2vec = DataFrame.
+//        word2vec = word2vec.omitNullRows();
+
 //        System.out.println(Arrays.toString(bagOfWords.features()));
-//        System.out.println(Arrays.toString(bagOfWords.apply(df.get(2, "V2").toString())));
+//        System.out.println(word2vec.summary());
+
+//        System.out.println();
+//        System.out.println(dictionary.size());
+
     }
 }
